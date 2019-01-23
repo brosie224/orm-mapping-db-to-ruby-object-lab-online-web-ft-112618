ad0d705c27c91a7cb245b3a14ec1ec4d5039128a
Brian Rosengarten <barosengarten@gmail.com>
2019-01-23 18:26:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,14 +16,14 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM students
+      SELECT name FROM students
       WHERE name = ?
     SQL
 
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end
-    
+
   end
 
   def save
