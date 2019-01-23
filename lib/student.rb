5a55da87a4fc5fe4da1d4bf14741bf3ed6b354b2
Brian Rosengarten <barosengarten@gmail.com>
2019-01-23 18:57:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,6 +30,17 @@ class Student
     end
   end
 
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students
